1a67996e0c3417163c83e0601b16f87f5bf92a59
cbfallesen <carolinefallesen@gmail.com>
2021-01-15 10:04:35 +0100

    fixet high score

--- jSpace-Project/src/tetris/HighScore.java
+++ jSpace-Project/src/tetris/HighScore.java
@@ -41,6 +41,7 @@ public class HighScore {
     public void addHighScore (String name, String score) {
 
         HighScoreData highScoreData = new HighScoreData(name, score);
+        boolean added = false;
 
         int i = 0;
 
@@ -54,13 +55,14 @@ public class HighScore {
                 if (Integer.parseInt(data.get(i).getScore()) > Integer.parseInt(score)) {
                     i++;
                 } else {
+                    added = true;
                     data.add(i, highScoreData);
                     writeHighScore();
                     break;
                 }
             }
 
-            if (data.size() < 10) {
+            if (data.size() < 10 && added == false) {
                 data.add((data.size()), highScoreData);
                 writeHighScore();
             }
