691ace783891a9090f33a33884f5935bd1435c68
Andreas Kanneworff <55582346+Kanneworff@users.noreply.github.com>
2021-01-18 09:51:19 +0100

    Login knap halløj

--- jSpace-Project/src/tetris/controller/LoginController.java
+++ jSpace-Project/src/tetris/controller/LoginController.java
@@ -5,6 +5,7 @@ import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.Pane;
@@ -20,14 +21,16 @@ public class LoginController {
     public LoginController() {}
 
     @FXML private Pane LoginPage;
+    @FXML private Pane NewUserPage;
     @FXML private TextField usernameTF;
     @FXML private PasswordField passwordPF;
+    @FXML private Button makeUserBtn;
+    @FXML private Button backBtn;
+
+
 
     @FXML
-    public void initialize() {
-        usernameTF.setPromptText("Username");
-        passwordPF.setPromptText("Password");
-    }
+    public void initialize() {}
 
 
     public void login(ActionEvent event) throws Exception {
@@ -46,5 +49,28 @@ public class LoginController {
         Scene scene = new Scene(root);
         stage.setScene(scene);
         stage.show();
+    }
+
+    @FXML
+    public void createUser(ActionEvent event) throws IOException {
+        Stage stage = (Stage) LoginPage.getScene().getWindow();
+        Parent root = FXMLLoader.load(getClass().getResource("/tetris/view/NewUserPage.fxml"));
+        Scene scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+    }
+
+
+    @FXML
+    public void back(ActionEvent event) throws IOException {
+        Stage stage = (Stage) NewUserPage.getScene().getWindow();
+        Parent root = FXMLLoader.load(getClass().getResource("/tetris/view/LoginPage.fxml"));
+        Scene scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    public void makeUser(ActionEvent event) {
+        //her
     }
 }
